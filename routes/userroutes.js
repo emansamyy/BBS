88cddbcfb05ee3d219fedcd5f1e46e342172e9d8
emansamyy <49169363+emansamyy@users.noreply.github.com>
2021-12-30 23:50:51 +0200

    comit

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,24 +1,22 @@
 // eman188849
 
-const express = require("express")
-const router = express.Router()
-const methodOverride = require('method-override');
-router.use(methodOverride('_method'));
+const express = require("express");
+const router = express.Router();
+const methodOverride = require("method-override");
+router.use(methodOverride("_method"));
 const {
-   
-    createUser,
-    updateUser,
-    deleteUser
+  createUser,
+  updateUser,
+  deleteUser,
+  checkuser,
+} = require("../controllers/ManageUsers");
 
-} = require ('../controllers/ManageUsers')
+router.get("/checkuser" , checkuser);
 
+router.post("/adduser", createUser);
 
-
-router.post('/adduser',     createUser)
-
-
-router.post('/deleteuser:UserID', deleteUser);
-router.delete('/deleteuser:UserID', deleteUser) 
+router.post("/deleteuser:UserID", deleteUser);
+router.delete("/deleteuser:UserID", deleteUser);
 
 // router.('/:UserID', updateUser)
-module.exports = router
+module.exports = router;
