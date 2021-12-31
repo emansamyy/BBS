d23c790a1a303d6ee122aecd621e58cf09e6c1bc
Apocryphon1 <46965861+Apocryphon1@users.noreply.github.com>
2021-12-31 18:14:28 +0200

    fix Conflivt userrouters

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-// eman188849
-
-const express = require("express");
-const router = express.Router();
-const methodOverride = require("method-override");
-router.use(methodOverride("_method"));
-const {
-  createUser,
-  updateUser,
-  deleteUser,
-} = require("../controllers/ManageUsers");
-=======
 const express = require("express")
 const router = express.Router()
 
@@ -21,29 +8,11 @@ const {
     signup,
 
 } = require ('../controllers/ManageUsers')
->>>>>>> f873c8018effb086c9339e3163eb723ddf219948
 
-router.post("/adduser", createUser);
-router.post("/deleteuser:UserID", deleteUser);
-router.delete("/deleteuser:UserID", deleteUser);
-
-// router.('/:UserID', updateUser)
-
-<<<<<<< HEAD
-//Omar's router
-const { createDonation } = require("../controllers/Managedonation");
-router.post("/addDonation", createDonation);
-=======
 router.post('/adduser',     addUser);
->>>>>>> f873c8018effb086c9339e3163eb723ddf219948
 
 router.post('/checkuser',     checkuser);
 
-<<<<<<< HEAD
-
-module.exports = router;
-=======
 router.post('/signup',     signup);
 
 module.exports = router
->>>>>>> f873c8018effb086c9339e3163eb723ddf219948
